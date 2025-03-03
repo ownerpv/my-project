ba21d80b45fefa3cc7c6ad8b119c769edbf8506f
Ahmad <97792988+ownerpv@users.noreply.github.com>
2025-03-04 03:04:36 +0330

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('scroll', () => {
-  const scrollY = window.scrollY;
-  const hero = document.querySelector('.hero');
-  const angle = 135 + (scrollY / window.innerHeight) * 45;
-  hero.style.background = `linear-gradient(${angle}deg, #000, #4B0082)`;
-});
