7d4f1162cc80ba62cf811b2f79b60088c82f4212
Ahmad <97792988+ownerpv@users.noreply.github.com>
2025-03-03 23:01:59 +0330

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("scroll", () => {
-    let scrollPosition = window.scrollY;
-    document.body.style.background = `rgb(${50 + scrollPosition / 5}, ${0 + scrollPosition / 10}, ${100 + scrollPosition / 5})`;
-});
